919d6b1063181534bf9238bbb8488d4aaf72b3ad
MarioMH8 <mhidalgomario@gmail.com>
2025-10-15 20:44:50 +0200

    feat(component): cambiar estilo del spinner

--- src/presentation/components/spinner/spinner.tsx
+++ src/presentation/components/spinner/spinner.tsx
@@ -5,23 +5,14 @@ import css from './spinner.module.css';
 const Spinner: FC<ComponentPropsWithRef<'svg'>> = properties => {
 	return (
 		<svg
-			{...properties}
 			viewBox='0 0 24 24'
-			xmlns='http://www.w3.org/2000/svg'>
-			<path
+			xmlns='http://www.w3.org/2000/svg'
+			{...properties}>
+			<circle
 				className={css.spinner}
-				d='M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z'
-				transform='translate(12, 12) scale(0)'
-			/>
-			<path
-				className={[css.spinner, css.delay_1].join(' ')}
-				d='M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z'
-				transform='translate(12, 12) scale(0)'
-			/>
-			<path
-				className={[css.spinner, css.delay_2].join(' ')}
-				d='M12,1A11,11,0,1,0,23,12,11,11,0,0,0,12,1Zm0,20a9,9,0,1,1,9-9A9,9,0,0,1,12,21Z'
-				transform='translate(12, 12) scale(0)'
+				cx='12'
+				cy='12'
+				r='0'
 			/>
 		</svg>
 	);
