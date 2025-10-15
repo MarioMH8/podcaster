d3045a7fe65c052cf891b09bc3fd791662d6d3f2
MarioMH8 <mhidalgomario@gmail.com>
2025-10-15 23:43:40 +0200

    feat: manejar error en la carga de podcast

--- src/presentation/context/top-podcast.hook.tsx
+++ src/presentation/context/top-podcast.hook.tsx
@@ -6,33 +6,39 @@ import { useEffect, useMemo, useState } from 'react';
 import { usePodcasterContext } from './podcaster.context';
 
 interface TopPodcastState {
+	isError: boolean;
 	isLoading: boolean;
 	podcasts: PodcastPrimitives[];
 }
 
 export default function useTopPodcast(): TopPodcastState {
 	const { isLoading, setLoading } = usePodcasterContext();
+	const [isError, setIsError] = useState<boolean>(false);
 	const [podcasts, setData] = useState<PodcastPrimitives[]>([]);
 	const useCase = useInjection(PodcastSearcher);
 
 	const fetchPodcasts = useMemo(() => {
 		return async () => {
+			setIsError(false);
 			setLoading(true);
 			try {
 				const query = new SearchPodcastQuery({ genre: 1310, limit: 100 });
 				const podcasts = await useCase.run(query);
 				setData(podcasts.map(p => p.toPrimitives()));
+			} catch {
+				setIsError(true);
 			} finally {
 				setLoading(false);
 			}
 		};
-	}, [useCase, setData, setLoading]);
+	}, [useCase, setData, setLoading, setIsError]);
 
 	useEffect(() => {
 		void fetchPodcasts();
 	}, [fetchPodcasts]);
 
 	return {
+		isError,
 		isLoading,
 		podcasts,
 	};
--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,15 +1,22 @@
 import { Spinner } from '@presentation/components';
 import { useTopPodcast } from '@presentation/context';
+import ErrorMessage from '@presentation/features/error-message';
 import type { FC } from 'react';
 import { Fragment } from 'react';
 
 import css from './home.module.css';
 
 const Home: FC = () => {
-	const { isLoading } = useTopPodcast();
+	const { isError, isLoading } = useTopPodcast();
 
 	if (isLoading) {
 		return <Spinner className={css.spinner} />;
+	}
+
+	if (isError) {
+		return (
+			<ErrorMessage>Hubo un error al cargar los podcasts. Por favor, intenta nuevamente más tarde.</ErrorMessage>
+		);
 	}
 
 	return <Fragment />;
