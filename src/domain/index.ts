587bf86c1ce82bb7e9acc5a28bb0149955b2facf
MarioMH8 <mhidalgomario@gmail.com>
2025-10-15 11:14:10 +0200

    feat(domain): crear index para exportar todo el dominio

---
+++ src/domain/index.ts
@@ -0,0 +1,8 @@
+export * from './criteria';
+export type { PodcastPrimitives } from './podcast';
+export { default as Podcast } from './podcast';
+export { default as PodcastAuthor } from './podcast-author';
+export { default as PodcastDescription } from './podcast-description';
+export { default as PodcastId } from './podcast-id';
+export { default as PodcastName } from './podcast-name';
+export { default as PodcastRepository } from './repository';
