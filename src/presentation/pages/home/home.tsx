1228f260e89e9041d298e4a702f509a14ff4456a
MarioMH8 <mhidalgomario@gmail.com>
2025-10-15 23:55:24 +0200

    feat: crear barra de filtrado de podcast

--- src/presentation/pages/home/home.tsx
+++ src/presentation/pages/home/home.tsx
@@ -1,4 +1,4 @@
-import { Spinner } from '@presentation/components';
+import { Input, Spinner } from '@presentation/components';
 import { useTopPodcast } from '@presentation/context';
 import ErrorMessage from '@presentation/features/error-message';
 import type { FC } from 'react';
@@ -7,7 +7,7 @@ import { Fragment } from 'react';
 import css from './home.module.css';
 
 const Home: FC = () => {
-	const { isError, isLoading } = useTopPodcast();
+	const { isError, isLoading, podcasts } = useTopPodcast();
 
 	if (isLoading) {
 		return <Spinner className={css.spinner} />;
@@ -19,7 +19,18 @@ const Home: FC = () => {
 		);
 	}
 
-	return <Fragment />;
+	return (
+		<Fragment>
+			<nav className={css.filter}>
+				<span className={css.badge}>{podcasts.length}</span>
+				<Input
+					className={css.input}
+					placeholder='Filter podcasts...'
+					type='search'
+				/>
+			</nav>
+		</Fragment>
+	);
 };
 
 Home.displayName = 'Home';
