94d5e8032628ddcdd8b0d18cfb39ee0f3e30fcb1
MarioMH8 <mhidalgomario@gmail.com>
2025-10-15 16:09:41 +0200

    test(application): incluir podcast mock

---
+++ mock/domain/index.ts
@@ -0,0 +1,31 @@
+import { Podcast } from '@domain';
+
+const PODCAST_1 = new Podcast({
+	author: 'Author 1',
+	description: 'Description 1',
+	id: 1,
+	name: 'Podcast 1',
+});
+
+const PODCAST_2 = new Podcast({
+	author: 'Author 2',
+	description: 'Description 2',
+	id: 2,
+	name: 'Podcast 2',
+});
+
+const PODCAST_3 = new Podcast({
+	author: 'Author 3',
+	description: 'Description 3',
+	id: 3,
+	name: 'Podcast 3',
+});
+
+const PODCAST_4 = new Podcast({
+	author: 'Author 4',
+	description: 'Description 4',
+	id: 4,
+	name: 'Podcast 4',
+});
+
+export { PODCAST_1, PODCAST_2, PODCAST_3, PODCAST_4 };
--- test/applicaction/search-podcast/search-podcast.use-case.test.ts
+++ test/applicaction/search-podcast/search-podcast.use-case.test.ts
@@ -1,35 +1,7 @@
 import { PodcastSearcher, SearchPodcastQuery } from '@application';
-import { Podcast, SearchPodcastCriteria } from '@domain';
-import { MockPodcastCacheRepository, MockPodcastRepository } from '@mock';
+import { SearchPodcastCriteria } from '@domain';
+import { MockPodcastCacheRepository, MockPodcastRepository, PODCAST_1, PODCAST_2, PODCAST_3, PODCAST_4 } from '@mock';
 import { beforeEach, describe, expect, it } from 'vitest';
-
-const PODCAST_1 = new Podcast({
-	author: 'Author 1',
-	description: 'Description 1',
-	id: 1,
-	name: 'Podcast 1',
-});
-
-const PODCAST_2 = new Podcast({
-	author: 'Author 2',
-	description: 'Description 2',
-	id: 2,
-	name: 'Podcast 2',
-});
-
-const PODCAST_3 = new Podcast({
-	author: 'Author 3',
-	description: 'Description 3',
-	id: 3,
-	name: 'Podcast 3',
-});
-
-const PODCAST_4 = new Podcast({
-	author: 'Author 4',
-	description: 'Description 4',
-	id: 4,
-	name: 'Podcast 4',
-});
 
 describe('PodcastSearcher', () => {
 	let cache: MockPodcastCacheRepository;
